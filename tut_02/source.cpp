cde46462fab6e129885a0547e41ab022b78ddb17
Adam Szymanowski <adam.szym@yahoo.pl>
2021-12-04 13:21:15 -0800

    Tutorial 02: How to scan images, lookup tables and time measurement with OpenCV

--- tut_02/source.cpp
+++ tut_02/source.cpp
@@ -9,13 +9,18 @@
 using namespace cv;
 using namespace std;
 
-Mat& ScanImageAndReduceC(Mat& I, const uchar* table);
+Mat& ScanImageAndReduceC(Mat& img, const uchar* const table);
+Mat& ScanImageAndReduceIterator(Mat& img, const uchar* const table);
+Mat& ScanImageAndReduceRandomAccess(Mat& img, const uchar* const table);
+const string func_name_1 = "ScanImageAndReduceC";
+const string func_name_2 = "ScanImageAndReduceIterator";
+const string func_name_3 = "ScanImageAndReduceRandomAccess";
 
 int main(int argc, char** argv)
 {
     cout << "How To Scan Images (OpenCV: " << CV_VERSION  << ")" << endl;
 
-    Mat original_img, reduced_img;
+    Mat original_img, reduced_img_1, reduced_img_2, reduced_img_3, reduced_img_4;
     string image_path = samples::findFile("starry_night.jpg");
     original_img = imread(image_path);
 
@@ -25,7 +30,7 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    int divideWith = 64;
+    int divideWith = 96;
 
     uchar table[256];
     for (int i = 0; i < 256; i++)
@@ -34,20 +39,75 @@ int main(int argc, char** argv)
     const int times = 100;
     double t;
 
+    // Function 1
     t = (double)getTickCount();
 
     for (int i = 0; i < times; i++)
     {
-        cv::Mat clone_img = original_img.clone();
-        reduced_img = ScanImageAndReduceC(clone_img, table);
+        Mat clone_img = original_img.clone();
+        reduced_img_1 = ScanImageAndReduceC(clone_img, table);
     }
 
     t = 1000 * (getTickCount() - t) / getTickFrequency();
-    cout << "Time of reducing with C operator [] (averaged for "
-        << times << " runs): " << t << " milliseconds." << endl;
+    t /= times;
+    cout << "Time of reducing with C operator [] (" << func_name_1 
+        << ") averaged for " << times << " runs: " << t << " milliseconds." << endl;
 
+    // Function 2
+    t =(double)getTickCount();
+
+    for (int i = 0; i < times; i++)
+    {
+        Mat clone_img = original_img.clone();
+        reduced_img_2 = ScanImageAndReduceIterator(clone_img, table);
+    }
+
+    t = 1000 * (getTickCount() - t) / getTickFrequency();
+    t /= times;
+    cout << "Time of reducing with the iterator (" << func_name_2 
+    << ") averaged for " << times << " runs: " << t << " milliseconds." << endl;
+
+    // Function 3
+    t = (double)getTickCount();
+
+    for (int i = 0; i < times; i++)
+    {
+        Mat clone_img = original_img.clone();
+        reduced_img_3 = ScanImageAndReduceRandomAccess(clone_img, table);
+    }
+
+    t = 1000 * (getTickCount() - t) / getTickFrequency();
+    t /= times;
+    cout << "Time of reducing with on-the-fly address generation - at function (" << func_name_2 
+    << ") averaged for " << times << " runs: " << t << " milliseconds." << endl;
+
+    // Function 4 (built-in LUT)
+    Mat look_up_table(1, 256, CV_8U);
+    uchar* p = look_up_table.ptr();
+    for (int i = 0; i < 256; ++i)
+        p[i] = table[i];
+
+    t = (double)getTickCount();
+
+    for (int i = 0; i < times; ++i)
+    {
+        Mat clone_img = original_img.clone();
+        LUT(clone_img, look_up_table, reduced_img_4);
+    }
+
+    t = 1000 * (getTickCount() - t) / getTickFrequency();
+    t /= times;
+    cout << "Time of reducing with LUT function"
+    << " averaged for " << times << " runs: " << t << " milliseconds." << endl;
+
+
+    // Display all the images
     imshow("Original Image", original_img);
-    imshow("Reduced Image", reduced_img);
+    imshow("Reduced Image by " + func_name_1, reduced_img_1);
+    imshow("Reduced Image by " + func_name_2, reduced_img_2);
+    imshow("Reduced Image by " + func_name_3, reduced_img_3);
+    imshow("Reduced Image by LUT", reduced_img_4);
+
     waitKey(0);
 
     return 0;
@@ -83,19 +143,70 @@ Mat& ScanImageAndReduceC(Mat& img, const uchar* const table)
     return img;
 }
 
-/*
+Mat& ScanImageAndReduceIterator(Mat& img, const uchar* const table)
+{
+    // Accept only char type matrices
+    CV_Assert(img.depth() == CV_8U);
+
+    const int channels = img.channels();
     switch (channels)
     {
         case 1:
         {
-
+            MatIterator_<uchar> it, end;
+            for (it = img.begin<uchar>(), end = img.end<uchar>(); it != end; ++it)
+                *it = table[*it];
         } break;
 
         case 3:
         {
-
+            MatIterator_<Vec3b> it, end;
+            for (it = img.begin<Vec3b>(), end = img.end<Vec3b>(); it != end; ++it)
+            {
+                (*it)[0] = table[(*it)[0]];
+                (*it)[1] = table[(*it)[1]];
+                (*it)[2] = table[(*it)[2]];
+            }
         } break;
         default:
-            cout << "switch default in " << "canImageAndReduceIterator" << "shouldn't be reached" << endl;
+            cout << "switch default in " << func_name_2 << "shouldn't be reached" << endl;
     }
-*/
+
+    return img;
+}
+
+Mat& ScanImageAndReduceRandomAccess(Mat& img, const uchar* const table)
+{
+    // Accept only char type matrices
+    CV_Assert(img.depth() == CV_8U);
+
+    const int channels = img.channels();
+    switch (channels)
+    {
+        case 1:
+        {
+            for (int i = 0; i < img.rows; ++i)
+                for (int j = 0; j <img.cols; ++j)
+                    img.at<uchar>(i, j) = table[img.at<uchar>(i, j)];
+        } break;
+
+        case 3:
+        {
+            Mat_<Vec3b> img_vec3 = img;
+            for (int i = 0; i < img.rows; ++i)
+            {
+                for (int j = 0; j < img.cols; ++j)
+                {
+                    img_vec3(i, j)[0] = table[img_vec3(i, j)[0]];
+                    img_vec3(i, j)[1] = table[img_vec3(i, j)[1]];
+                    img_vec3(i, j)[2] = table[img_vec3(i, j)[2]];
+                }
+            }
+            img = img_vec3; 
+        } break;
+        default:
+            cout << "switch default in " << func_name_3 << "shouldn't be reached" << endl;
+    }
+
+    return img;
+}
